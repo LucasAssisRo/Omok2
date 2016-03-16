12622e4f70c7058c34b7d73c760ab6d27524378c
LucasAssisRo <lucas.assis.ro@gmail.com>
2016-03-15 21:11:54 -0600

    created classes with no code just for reference

--- app/src/main/java/edu/utep/cs/cs4330/hw3/omok/control/GameActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/hw3/omok/control/GameActivity.java
@@ -1,13 +1,12 @@
-package edu.utep.cs.cs4330.hw3.omok;
+package edu.utep.cs.cs4330.hw3.omok.control;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/Strategy.java
@@ -0,0 +1,7 @@
+package edu.utep.cs.cs4330.hw3.omok.model;
+
+/**
+ * Created by lucasassisrodrigues on 3/15/16.
+ */
+public interface Strategy {
+}
