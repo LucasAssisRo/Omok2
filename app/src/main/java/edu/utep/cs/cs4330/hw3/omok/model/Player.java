7aa957220f5ff3e23ff1c185e452acda926756b8
juanrazo <jmrazo@miners.utep.edu>
2016-03-20 12:42:42 -0600

    Updated Boad, Coordinates and Player

--- app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/Player.java
+++ app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/Player.java
@@ -1,7 +1,37 @@
 package edu.utep.cs.cs4330.hw3.omok.model;
 
-/**
- * Created by lucasassisrodrigues on 3/15/16.
- */
 public class Player {
+    private String name;
+    private char stone;
+    private boolean playerOne;
+    
+    public Player(){
+        name = "First Player";
+        stone = 'B';
+        playerOne = true;
+    }
+    
+    public Player(String name, int num){
+        this.name=name;
+        if(num==1){
+            stone='B';
+            playerOne=true;
+        }
+        else{
+            stone='W';
+            playerOne=false;
+        }
+    }
+    
+    public char getStone(){
+        return stone;
+    }
+    
+    public boolean getPlayerOne(){
+        return playerOne;
+    }
+    
+    public String getName(){
+        return name;
+    }
 }
