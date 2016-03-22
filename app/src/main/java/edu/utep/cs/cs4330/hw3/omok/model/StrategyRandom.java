f60afd0d367ff74dfab70b23e1f22b19bd884c05
juanrazo <jmrazo@miners.utep.edu>
2016-03-21 23:48:04 -0600

    Added Random & Part of Smart

--- app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/StrategyRandom.java
+++ app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/StrategyRandom.java
@@ -1,7 +1,16 @@
 package edu.utep.cs.cs4330.hw3.omok.model;
+import java.util.Random;
 
-/**
- * Created by lucasassisrodrigues on 3/15/16.
- */
-public class StrategyRandom {
+public class StrategyRandom implements Strategy{
+    Random xCoor = new Random();
+    Random yCoor = new Random();
+
+
+    @Override
+    public Coordinates play(Board board) {
+        Coordinates coordinates = new Coordinates();
+        coordinates.setX(xCoor.nextInt(10));
+        coordinates.setY(yCoor.nextInt(10));
+        return coordinates;
+    }
 }
--- app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/StrategySmart.java
+++ app/src/main/java/edu/utep/cs/cs4330/hw3/omok/model/StrategySmart.java
@@ -3,6 +3,75 @@ package edu.utep.cs.cs4330.hw3.omok.model;
 /**
  * Created by lucasassisrodrigues on 3/15/16.
  */
-public class StrategySmart {
+public class StrategySmart implements Strategy{
+    Coordinates coordinates = new Coordinates();
 
+    @Override
+    public Coordinates play(Board board) {
+        checkBoard(board);
+        return coordinates;
+    }
+
+    public void checkBoard(Board board){
+        int east = 0;
+        int south = 0;
+        int southWest = 0;
+        int southEast = 0;
+        int temp = 0;
+        int max = 0;
+        for(int x = 0;x < board.getBoard().length; x++ ){
+            for(int y=0; y< board.getBoard().length; y++){
+                if(board.getBoard()[x][y]=='W'){
+                    temp = board.east(x, y, 'W');
+                    east = (temp>east) ? temp : east;
+                    temp = board.south(x, y, 'W');
+                    south = (temp>south) ? temp : south;
+                    temp = board.southWest(x, y, 'W');
+                    southWest = (temp>southWest) ? temp : southWest;
+                    temp = board.southEast(x, y, 'W');
+                    southEast = (temp>southEast) ? temp : southEast;
+                }
+                max = (east>max) ? east: max;
+                max = (south>max) ? south : max;
+                max = (southWest>max) ? southWest : max;
+                max = (southEast>max) ? southEast : max;
+                if(max>3){
+                    coordinates.setX(x);
+                    coordinates.setY(y);
+                }
+            }
+        }
+    }
+
+    public boolean verifyEast(int x, int y){
+
+        if((x+1) <10){
+            coordinates.setX(x + 1);
+            coordinates.setY(y);
+            return true;
+        }
+
+        if((x-1) >-1 && (y-1) >-1){
+            coordinates.setX(x-1);
+            coordinates.setY(y);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean south(int x, int y){
+
+        if((x+1) <10){
+            coordinates.setX(x + 1);
+            coordinates.setY(y+1);
+            return true;
+        }
+
+        if((x-1) >-1){
+            coordinates.setX(x-1);
+            coordinates.setY(y);
+            return true;
+        }
+        return false;
+    }
 }
